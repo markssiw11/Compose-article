bd20da764701dc615ac60d807d386a1218887aa6
long nguyen <longnguyen95.xn@gmail.com>
2022-11-09 10:40:51 +0700

    add Compose article app

---
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.jetpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun JetpackComposeBody() {
+    Column {
+        Text(
+            text = "Jetpack Compose Tutorial",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(text = stringResource(R.string.title_1),
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.title_2),
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun HeaderImage() {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column() {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+            )
+        JetpackComposeBody()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTutorialTheme {
+        HeaderImage()
+    }
+}
